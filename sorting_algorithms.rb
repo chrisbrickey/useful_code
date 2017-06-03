acd1c180d91d46a65d12eab3f612110887f57cf0
chrisbrickey <cbrickey1@gmail.com>
2017-06-03 15:04:21 -0700

    bubble_sort long form refactored

--- sorting_algorithms.rb
+++ sorting_algorithms.rb
@@ -15,21 +15,18 @@ my_arr = [6, 9, 3, 67, 0, 12, 5, 8, 3]
 # p bubble_sort_short!(my_arr) { |x, y| x <=> y }
 
 
-
-
-
-def bubble_sort_long! (arr)
+def bubble_sort_short! (arr)
   length = arr.length
   return arr if length <= 1
-
-
 
   loop do
     swapped = false
 
     0.upto(length - 2) do |idx|
-      if arr[idx] > arr[idx + 1]
-        arr[idx], arr[idx + 1] = arr[idx + 1], arr[idx]
+      x = idx
+      y = idx + 1
+      if arr[x] > arr[y]
+        arr[x], arr[y] = arr[y], arr[x]
         swapped = true
       end
     end
@@ -40,4 +37,27 @@ def bubble_sort_long! (arr)
 end
 
 
-p bubble_sort_long!(my_arr)
+
+def bubble_sort_long! (arr)
+  length = arr.length
+  return arr if length <= 1
+
+  loop do
+    swapped = false
+
+    0.upto(length - 2) do |idx|
+      x = idx
+      y = idx + 1
+      if arr[x] > arr[y]
+        arr[x], arr[y] = arr[y], arr[x]
+        swapped = true
+      end
+    end
+    break if swapped == false
+  end
+
+  arr
+end
+
+
+p bubble_sort_short!(my_arr)
